ed43dc90b209ca8d4eed83b4e21f97703109efd4
Andreas Helmberger <public@ahelmberger.de>
2015-10-16 16:58:48 +0200

    Switch from superagent to node-fetch

--- lc-sdk-node.js
+++ lc-sdk-node.js
@@ -1,8 +1,5 @@
-var q       = require('q');
 var extend  = require('extend');
-var request = require('superagent');
-
-require('q-superagent')(request);
+var fetch   = require('node-fetch');
 
 var DEFAULT_CONFIG = {
   discoveryServers: [],
@@ -45,13 +42,15 @@ module.exports = function (cfg) {
       // Map url to a list of operations
       var funcs = urls.map(function (url) {
         return function () {
-          return request(method, 'http://' + url + path)
-            .set('Connection', 'keep-alive')
-            .send(value)
-            .type('json')
-            .accept('json')
-            .timeout(config.timeout)
-            .q();
+          return fetch('http://' + url + path, {
+            method: method,
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'Connection': 'keep-alive'
+            },
+            body: value
+          }).then(handleResponse);
         };
       });
 
@@ -59,20 +58,27 @@ module.exports = function (cfg) {
     });
   }
 
+  function handleResponse(res) {
+    if (res.status < 200 || res.status > 299) {
+      throw new Error(res);
+    }
+    if (res.status !== 204) {
+      return res.json();
+    }
+  }
+
   function getServices(tag){
 
     var funcs = config.discoveryServers.map(function (discoveryServer) {
       return function () {
-        return request
-          .get(discoveryServer + '/v1/catalog/services')
-          .timeout(config.timeout)
-          .q();
+        return fetch(discoveryServer + '/v1/catalog/services')
+          .then(handleResponse);
       };
     });
 
     return invokeUntilResolved(funcs).then(function (result) {
-      return Object.keys(result.body).filter(function(key){
-        return tag ? result.body[key].indexOf(tag) !== -1 : key;
+      return Object.keys(result).filter(function(key){
+        return tag ? result[key].indexOf(tag) !== -1 : key;
       });
     });
   }
@@ -80,17 +86,17 @@ module.exports = function (cfg) {
   function getServiceUrlsByTag(tag){
     return getServices(tag)
       .then(function(result){
-        return q        
+        return q
           .all(result.map(function (name) {
             return getServiceUrls(name)
               .then(function(data){
                 var result = {};
                 result[name] = data;
                 return result;
-              }); 
+              });
           }))
           .then(function(data){
-            var result = {}; 
+            var result = {};
             data.forEach(function(itm){
               extend(result, itm);
             });
@@ -100,19 +106,17 @@ module.exports = function (cfg) {
   }
 
   function getServiceUrls(serviceName) {
-    if (config.services[serviceName]) return q.when(config.services[serviceName]);
+    if (config.services[serviceName]) return Promise.when(config.services[serviceName]);
 
     var funcs = config.discoveryServers.map(function (discoveryServer) {
       return function () {
-        return request
-          .get(discoveryServer + '/v1/catalog/service/' + serviceName)
-          .timeout(config.timeout)
-          .q();
+        return fetch(discoveryServer + '/v1/catalog/service/' + serviceName)
+          .then(handleResponse);
       };
     });
 
     return invokeUntilResolved(funcs).then(function (result) {
-      var serviceUrls = result.body.map(function (itm) {
+      var serviceUrls = result.map(function (itm) {
         return itm.Address + ':' + itm.ServicePort;
       });
 
@@ -124,7 +128,7 @@ module.exports = function (cfg) {
   function invokeUntilResolved(funcs) {
     return funcs.reduce(function (previous, next) {
       return previous.catch(next);
-    }, q.reject(new Error('No function specified')));
+    }, Promise.reject(new Error('No function specified')));
   }
 
 };
--- lc-sdk-node.specs.js
+++ lc-sdk-node.specs.js
@@ -1,16 +1,20 @@
 var serviceSDK = require('./lc-sdk-node');
 var expect     = require('chai').expect;
 
-var DISCOVERY_SERVICE_URLS = ['http://46.101.138.192:8500', 'http://46.101.191.124:8500'];
+var DISCOVERY_SERVICE_URLS = [
+  'http://46.101.245.190:8500',
+  'http://46.101.132.55:8500',
+  'http://46.101.193.82:8500'
+];
 
 describe('When we make a request to a service', function () {
   var result;
 
   before(function (done) {
     serviceSDK({ discoveryServers: DISCOVERY_SERVICE_URLS })
-      .get('cover-service', '/healthcheck')
+      .get('welcome-service', '/healthcheck')
       .then(function (res) {
-        result = res.body;
+        result = res;
       })
       .then(done, done);
   });
